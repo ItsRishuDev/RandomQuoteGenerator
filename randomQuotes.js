4b31db14257b92433684e1ace964cdb18b0c1b63
Rishabh Soni <54630851+ItsRishuDev@users.noreply.github.com>
2020-09-02 15:17:43 +0530

    Update randomQuotes.js

--- randomQuotes.js
+++ randomQuotes.js
@@ -1,7 +1,3 @@
-let beginningRandom = Math.floor(Math.random() * 10)
-let middleRandom = Math.floor(Math.random() * 10)
-let endRandom = Math.floor(Math.random() * 10)
-
 let beginningQuote = ['Journey', 'Enjoyment', 'Movies', 'Happiness', 'Fun', 'Quotes', 'Adventure', 'Travel', 'Games', 'Music'];
 let middleQuote = ['was designed by', 'was introduced with', 'is like', 'is a flavour in', 'is released with', 'is your', 'is important for', 'describes','released', 'is much better for'];
 let endQuote = ['life', 'ourself', 'experience', 'enjoyment', 'friend', 'love', 'emotion', 'creation', 'you', 'experties'];
@@ -10,5 +6,9 @@ let endQuote = ['life', 'ourself', 'experience', 'enjoyment', 'friend', 'love', 
 //document.getElementById('generate'),addEventListener('click', generate);
 
 function generate() {
+    let beginningRandom = Math.floor(Math.random() * 10)
+    let middleRandom = Math.floor(Math.random() * 10)
+    let endRandom = Math.floor(Math.random() * 10)
+
     document.getElementById('quote').innerHTML = beginningQuote[beginningRandom] + ' ' + middleQuote[middleRandom] + ' ' + endQuote[endRandom] + '.';
 }
